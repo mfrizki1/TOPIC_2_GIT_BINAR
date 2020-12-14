20686d3e0f61631dca561df3036044f463976434
berkahsuksesteknologi <mfrizki1>
2020-12-14 22:48:04 +0800

    Added Home Calculator

---
+++ src/menu_cal.kt
@@ -0,0 +1,29 @@
+fun main() {
+    //BRANCH MAIN
+    println("Kalkulator")
+    println("====================================================")
+    println("Pilih : ")
+    println("1. Tambah")
+    println("2. Kurang")
+    println("3. Kali")
+    println("4. Bagi")
+    print("Pilih Menu :")
+    val pick_menu = readLine()
+    when(pick_menu){
+        "1"->{
+
+        }
+        "2"->{
+
+        }
+        "3"->{
+
+        }
+        "4"->{
+
+        }
+        else->{
+            print("Tidak ada menu")
+        }
+    }
+}
